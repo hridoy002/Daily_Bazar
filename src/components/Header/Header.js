7f9c072c45a648c5062afe6a671cdc4ffda3eb04
hridoy002 <hridoyalhasan2@gmail.com>
2022-04-05 13:02:31 +0600

    install router & make components

---
+++ src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <nav>
+            <a href=""></a>Home
+            <a href="">Reviews</a>
+            <a href="">Blogs</a>
+            <a href="">About Us</a>
+        </nav>
+    );
+};
+
+export default Header;
