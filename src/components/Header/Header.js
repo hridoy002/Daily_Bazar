2282cab5dd958c6fb56b17b4947ae55cc551f1fe
hridoy002 <hridoyalhasan2@gmail.com>
2022-04-05 14:27:51 +0600

    route link

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Header = () => {
     return (
-        <nav>
+        <nav className='sticky top-0 bg-sky-500 px-12 py-8 flex justify-center md:justify-between'>
             <a href=""></a>Home
             <a href="">Reviews</a>
             <a href="">Blogs</a>
